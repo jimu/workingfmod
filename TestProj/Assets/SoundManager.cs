8fe9c20c81528da778c31553f5062718d304275e
Gerald Clark <farthestempire@gmail.com>
2020-10-20 21:04:59 -0500

    good lord

---
+++ TestProj/Assets/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager mainAudio;
+
+    public string gunshot;
+
+    FMOD.Studio.EventInstance gunshotEvent;
+
+    void Awake()
+    {
+        if (mainAudio != null)
+        {
+            DestroyImmediate(this);
+            return;
+        }
+
+        mainAudio = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        gunshotEvent = FMODUnity.RuntimeManager.CreateInstance(gunshot);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            gunshotEvent.start();
+        }
+    }
+}
